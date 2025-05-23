4e75a9ee8e71c8cceb09dd075d4ac4d960b60b69
Admin-verinsure <admin@verinsure.online>
2025-05-23 15:18:28 +1200

    chaned whole res_user file from ldap module copy from not4profit

--- addons/ldap_reset_password/res_users.py
+++ addons/ldap_reset_password/res_users.py
@@ -8,9 +8,14 @@ _logger = logging.getLogger(__name__)
 
 class ResUsers(models.Model):
     _inherit = 'res.users'
+    #changed code here
+    last_logged_ip = fields.Char(string="Last Logged IP", readonly=True)
+    last_logged_browser = fields.Char(string="Last Logged Browser", readonly=True)
+    last_logged_os = fields.Char(string="Last Logged OS", readonly=True)
 
     @classmethod
-    def authenticate(cls, db, login, password, user_agent_env):
+    def authenticate(cls, db, login, password):
+        user_agent_env = None #chnaged code here
         res = cls._login(db, login, password)
         if isinstance(res, tuple):
             uid, _ = res
